fa567ab02706de0021502a54401edd33ed6962cd
Joe Flanigan aka Rebirth <mert.torun97@outlook.de>
2015-04-12 12:24:57 +0200

    Create Auth.c

---
+++ source-files/Auth.c
@@ -0,0 +1,17 @@
+#include "MasterHandler.h"
+
+void SG_AUTH_SYS(void) {
+	// Soon(TM)
+}
+
+void SG_AUTH_SYS_INPUT_USERNAME(void) {
+	// Soon(TM)
+}
+
+void SG_AUTH_SYS_INPUT_PW(void) {
+  // Soon(TM)
+}
+
+void SG_AUTH_SYS_OPERATION(void) {
+	// Soon(TM)
+}
