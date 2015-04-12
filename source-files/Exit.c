ec00923198b0f32453fbbdffecc890532c3bcf86
Joe Flanigan aka Rebirth <mert.torun97@outlook.de>
2015-04-12 13:04:39 +0200

    Update Exit.c

--- source-files/Exit.c
+++ source-files/Exit.c
@@ -1,18 +1,18 @@
 #include "MasterHandler.h"
 
-/*
- *					    PJ; SNOWGLOBE -RB
- *				   	<===|===><==|==><===|===>
- *
- *		File Name		=> Exit.c
- *		Author  Name		=> "Rebirth" aka "Joe Flanigan"
- *		File Description	=> N/A
- */
+//                       PJ; SNOWGLOBE -RB
+//                   <===|===><==|==><===|===>
+//                            
+//     File Name            => Exit.c
+//     Author Name          => "Rebirth" aka "Joe Flanigan"
+//                            
+//     File Description	    => Surprise, surprise. It will serve with exit options
+//                             for this sweet tool
 
 unsigned int STD_EXIT(void) {
 	TITLES();
 	printf("	Leaving Project Snowglobe, thank you for using it.\n");
 	printf("	~Joe Flanigan\n");
-	Sleep(AU);														// Wait a couple of seconds before closing the console
-	exit(EXIT_SUCCESS);													// We could also use, return (0); here. Doesn't matter.
+	Sleep(AU);
+	exit(EXIT_SUCCESS);
 }
