77a9eefd8eb3d8801ec158d6e8187be5f7d2e270
Joe Flanigan aka Rebirth <mert.torun97@outlook.de>
2015-04-12 12:22:53 +0200

    Create Input_Ctrl.c

---
+++ source-files/Input_Ctrl.c
@@ -0,0 +1,60 @@
+#include "MasterHandler.h"
+
+void MAIN_MENU_INPUT_CTRL(char ov_menu_input) {
+	if ((ov_menu_input != 'e' && ov_menu_input != 'c')
+		&& (ov_menu_input != 'a' && ov_menu_input != 'm')) BB_ERROR();
+}
+
+bool CALCULATOR_INPUT_ROP_CTRL(char c_input_rop, bool CHECK) {
+	if ((c_input_rop != '+' && c_input_rop != '-')
+		&& (c_input_rop != '*' && c_input_rop != '/')
+		&& (c_input_rop != '%' && c_input_rop != 's')) {
+		BB_ERROR();
+		CHECK = TRUE;
+	}
+	if (CHECK != FALSE) TITLES();
+
+	return (CHECK);
+}
+
+bool CALCULATOR_INPUT_CTRL(unsigned int c_input, bool CHECK) {
+	if (c_input > CLM || c_input < CLN) {
+		BB_ERROR();
+		CHECK = TRUE;
+	}
+	if (CHECK != FALSE) TITLES();
+
+	return (CHECK);
+}
+
+bool ATM_INPUT_CTRL(float atm_input, bool CHECK) {
+	if (atm_input > ALM || atm_input < ALN) {
+		BB_ERROR();
+		CHECK = TRUE;
+	}
+	if (CHECK != FALSE) TITLES();
+
+	return (CHECK);
+}
+
+void MAINTENANCE_CENTER_MENU_INPUT_CTRL(char ov_menu_input) {
+	if ((ov_menu_input != 'e' && ov_menu_input != 'r')
+		&& (ov_menu_input != 'c' && ov_menu_input != 'a')
+		&& (ov_menu_input != 't')) BB_ERROR();
+}
+
+bool SUB_MENU_REPEAT_CTRL(char ov_menu_input, bool CHECK) {
+	if ((ov_menu_input != 'e' && ov_menu_input != 'r')
+		&& (ov_menu_input != 't')) {
+		BB_ERROR();
+		CHECK = TRUE;
+	}
+	if (CHECK != FALSE) TITLES();
+
+	return (CHECK);
+}
+
+void CELSIUS_FAHRENHEIT_MISCTOOL_MENU_CTRL(char ov_menu_input) {
+	if ((ov_menu_input != 'e' && ov_menu_input != 't')
+		&& (ov_menu_input != 'c' && ov_menu_input != 'f')) BB_ERROR();
+}
