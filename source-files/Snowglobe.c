9ed8f4f3bee239fe88d1a496536142b9a7e08c34
Joe Flanigan aka Rebirth <mert.torun97@outlook.de>
2015-04-12 12:17:36 +0200

    Update Snowglobe.c

--- source-files/Snowglobe.c
+++ source-files/Snowglobe.c
@@ -1,1 +1,27 @@
-// wait once again..
+#include "MasterHandler.h" // Let's include our headers, nu?
+
+/*		
+ *						PJ; SNOWGLOBE -RB
+ *				   	<===|===><==|==><===|===>
+ *
+ *      Project Name			=> "Project Snowglobe"
+ *		Author  Name			=> "Rebirth" aka "Joe Flanigan"
+ *		Project Output Language => "English"
+ *								   - A couple of comments, within this source code
+ *									 could be in german of course.
+ *
+ *		Project Description		=> "PJ; Snowglobe is my Sandbox, really it actually has no use."
+ */
+
+void SNOWGLOBE(void) { 
+	char ov_menu_input = NI, *cck = NULL;
+	bool CHECK = NI;
+
+	ShinyIntro();
+
+	do {
+		BOOL_CHECK();
+		ov_menu_input = MAIN_MENU(ov_menu_input);
+		ov_menu_input = MAIN_OPERATION(ov_menu_input, CHECK);
+	} while (ov_menu_input == 't');
+}
